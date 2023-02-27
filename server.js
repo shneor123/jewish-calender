21a853d3fbb78ec17953554e9fff3b6d2bbcd86f
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2023-02-27 14:34:15 +0200

    deploy

--- server.js
+++ server.js
@@ -1,48 +1,12 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const Hebcal = require('hebcal')
-
-const app = express()
-
-app.use(cookieParser())
-app.use(express.json())
-
-const convertToJewishDate = (gregorianDate) => {
-    const regex = /^(\d{1,2})[-.,/]?(\d{1,2})[-.,/]?(\d{4})$/
-    const match = gregorianDate.match(regex)
-    if (match) {
-        const [, month, day, year] = match
-        const hebrewDate = new Hebcal.HDate(new Date(year, month - 1, day || 1))
-        return hebrewDate.toString('h')
-    } else {
-        throw new Error('Invalid date format. Please enter a date in the format MM/DD/YYYY, MM-DD-YYYY, or MM.DD.YYYY.')
-    }
-}
-
-
-app.get('/bot/dates', (req, res) => {
-    var d = req.query.d
-    var dates = req.query.dates
-    const jewishDate = convertToJewishDate(d);
-    console.log('d', d)
-    const scoreData = { "actions": [{ "type": "SendMessage", "text": jewishDate + ' -התאריך המקביל' }] }
-    res.json(scoreData)
-})
-
-const port = 3030
-app.listen(port, () =>
-    console.log(`Server is ready at ${port}`)
-)
-
-
 // const express = require('express')
 // const cors = require('cors')
 // const path = require('path')
 // const cookieParser = require('cookie-parser')
 // const Hebcal = require('hebcal')
+// const axios = require('axios')
 
 // const app = express()
-// const http = require('http').createServer(app)
+// app.use(express.json())
 
 // // Config the Express App
 // app.use(express.static('public'))
@@ -66,6 +30,8 @@ app.listen(port, () =>
 // const botRoutes = require('./api/bot/bot.routes')
 // app.use('/api/bot', botRoutes)
 
+
+
 // const convertToJewishDate = (gregorianDate) => {
 //     const regex = /^(\d{1,2})[-.,/]?(\d{1,2})[-.,/]?(\d{4})$/
 //     const match = gregorianDate.match(regex)
@@ -78,22 +44,109 @@ app.listen(port, () =>
 //     }
 // }
 
+// const getTorahPortion = async (jewishData) => {
+//     const currentDate = new Date();
+//     const currentYear = jewishData ? currentDate.getFullYear() : ''
+//     const currentMonth = jewishData ? currentDate.getMonth() + 1 : ''
+//     const currentDay = jewishData ? currentDate.getDate() : ''
+//     const apiSefria = `https://www.sefaria.org/api/calendars?timezone=Asia/Jerusalem&forward=20&lang=he${jewishData ? `&gy=${currentYear}&gm=${currentMonth}&gd=${currentDay}` : ''}`
+//     try {
+//         const { data: sefariaData } = await axios.get(apiSefria)
+//         const torahPortionItem = sefariaData.calendar_items.find((item) => item.category === 'Tanakh')
+//         if (torahPortionItem && torahPortionItem.displayValue) {
+//             const { he } = torahPortionItem.displayValue
+//             const torahPortionStrHe = `פרשת השבוע: ${he}`
+//             return torahPortionStrHe
+//         } else {
+//             return 'Unable to retrieve Torah portion for this week.'
+//         }
+//     } catch (error) {
+//         console.error(error)
+//         return 'Error: Unable to retrieve Torah portion.'
+//     }
+// }
 
-// app.get('/bot/dates', (req, res) => {
-//     var d = req.query.d
-//     var dates = req.query.dates
-//     const jewishDate = convertToJewishDate(d);
-//     console.log('d', d)
-//     const scoreData = { "actions": [{ "type": "SendMessage", "text": jewishDate + ' -התאריך המקביל' }] }
-//     res.json(scoreData)
-// })
-
-
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+// app.get('/bot/dates', async (req, res) => {
+//     try {
+//         const d = req.query.d
+//         const jewishDate = convertToJewishDate(d)
+//         const jewishParash = await getTorahPortion(true) // assuming you want the Torah portion in Hebrew
+//         const scoreData = {
+//             "actions": [{ "type": "SendMessage", "text": `התאריך המקביל:-${jewishDate}  ${jewishParash}` }]
+//         }
+//         res.json(scoreData)
+//     } catch (error) {
+//         console.error(error)
+//         res.status(500).send('An error occurred while retrieving the Torah portion.')
+//     }
 // })
 
 // const port = 3030
 // app.listen(port, () =>
 //     console.log(`Server is ready at ${port}`)
 // )
+
+
+const express = require('express')
+const Hebcal = require('hebcal')
+const axios = require('axios')
+
+const app = express()
+app.use(express.json())
+
+const convertToJewishDate = (gregorianDate) => {
+    const regex = /^(\d{1,2})[-.,/]?(\d{1,2})[-.,/]?(\d{4})$/
+    const match = gregorianDate.match(regex)
+    if (match) {
+        const [, month, day, year] = match
+        const hebrewDate = new Hebcal.HDate(new Date(year, month - 1, day || 1))
+        return hebrewDate.toString('h')
+    } else {
+        throw new Error('Invalid date format. Please enter a date in the format MM/DD/YYYY, MM-DD-YYYY, or MM.DD.YYYY.')
+    }
+}
+
+const getTorahPortion = async (jewishData) => {
+    const currentDate = new Date();
+    const currentYear = jewishData ? currentDate.getFullYear() : ''
+    const currentMonth = jewishData ? currentDate.getMonth() + 1 : ''
+    const currentDay = jewishData ? currentDate.getDate() : ''
+    const apiSefria = `https://www.sefaria.org/api/calendars?timezone=Asia/Jerusalem&forward=20&lang=he${jewishData ? `&gy=${currentYear}&gm=${currentMonth}&gd=${currentDay}` : ''}`
+    try {
+        const { data: sefariaData } = await axios.get(apiSefria)
+        const torahPortionItem = sefariaData.calendar_items.find((item) => item.category === 'Tanakh')
+        if (torahPortionItem && torahPortionItem.displayValue) {
+            const { he } = torahPortionItem.displayValue
+            const torahPortionStrHe = `פרשת השבוע: ${he}`
+            return torahPortionStrHe
+        } else {
+            return 'Unable to retrieve Torah portion for this week.'
+        }
+    } catch (error) {
+        console.error(error)
+        return 'Error: Unable to retrieve Torah portion.'
+    }
+}
+
+
+app.get('/bot/dates', async (req, res) => {
+    try {
+        const d = req.query.d
+        const jewishDate = convertToJewishDate(d)
+        const jewishParash = await getTorahPortion(true) // assuming you want the Torah portion in Hebrew
+        const scoreData = {
+            "actions": [{ "type": "SendMessage", "text": `התאריך המקביל:-${jewishDate}  ${jewishParash}` }]
+        }
+        res.json(scoreData)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('An error occurred while retrieving the Torah portion.')
+    }
+})
+
+
+const port = 3030
+app.listen(port, () =>
+    console.log(`Server is ready at ${port}`)
+)
+
--- services/jewish-convert.js
+++ services/jewish-convert.js
@@ -5,14 +5,14 @@ class BotService {
 
     async convertToJewishDate(gregorianDate) {
         try {
-            const regex = /^(\d{1,2})[-.,/]?(\d{1,2})[-.,/]?(\d{4})$/;
-            const match = gregorianDate.match(regex);
+            const regex = /^(\d{1,2})[-.,/]?(\d{1,2})[-.,/]?(\d{4})$/
+            const match = gregorianDate.match(regex)
             if (match) {
-                const [, month, day, year] = match;
-                const hebrewDate = new Hebcal.HDate(new Date(year, month - 1, day || 1));
-                return hebrewDate.toString('h');
+                const [, month, day, year] = match
+                const hebrewDate = new Hebcal.HDate(new Date(year, month - 1, day || 1))
+                return hebrewDate.toString('h')
             } else {
-                throw new Error('Invalid date format. Please enter a date in the format MM/DD/YYYY, MM-DD-YYYY, or MM.DD.YYYY.');
+                throw new Error('Invalid date format. Please enter a date in the format MM/DD/YYYY, MM-DD-YYYY, or MM.DD.YYYY.')
             }
         } catch (err) {
             throw new Error(`Failed to convert date: ${err.message}`);
@@ -20,8 +20,8 @@ class BotService {
     }
 }
 
-const botService = new BotService()
+const jewishConvertService = new BotService()
 
 module.exports = {
-    botService
+    jewishConvertService
 }
