e9a19e63fc794dd8aa3ea2ff66413bc70cf7a804
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2023-02-28 14:00:47 +0200

    its work convert to date now we need to use my server no ngork

--- server.js
+++ server.js
@@ -1,21 +1,43 @@
 const express = require('express')
 const Hebcal = require('hebcal')
+const moment = require('moment')
 
 const app = express()
 app.use(express.json())
 
-app.get('/bot/dates', async (req, res) => {
-    try {
-        const userInputDate = req.query.d
-        const date = new Date(userInputDate)
-        const hebDate = Hebcal.HDate(date)
-        const torahPortion = hebDate.getSedra('he', true)
 
-        const responseText = `התאריך המקביל: ${hebDate.toString('h')}  ${torahPortion}`
+
+app.post('/bot/dates/post', async (req, res) => {
+    try {
+        const hebDateString = req.body.parameters.find(p => p.name === 'תאריך עברי').value
+        const hebDate = moment(hebDateString, 'DDMMYYYY').toDate()
+        const hebDateStr = Hebcal.HDate(hebDate).toString('h')
+        const responseText = `${hebDateStr}`
         const responseData = {
             "actions": [{ "type": "SendMessage", "text": responseText }]
         }
         res.json(responseData)
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('An error occurred while retrieving the Torah portion.')
+    }
+})
+
+
+app.get('/bot/dates', async (req, res) => {
+    try {
+        const userInputDate = req.query.d
+        console.log("🚀  ~ userInputDate:", userInputDate)
+        const date = new Date(userInputDate)
+        const hebDate = Hebcal.HDate(date)
+        const torahPortion = hebDate.getSedra('he', true)
+
+        const responseText = ` ${torahPortion} התאריך המקביל: ${hebDate.toString('h')}`
+        const responseData = {
+            "actions": [{ "type": "SendMessage", "text": responseText }]
+        }
+        res.json(responseData)
+        console.log(responseData)
     } catch (error) {
         console.error(error)
         res.status(500).send('An error occurred while retrieving the Torah portion.')
@@ -31,74 +53,4 @@ app.listen(port, () =>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // http://localhost:3030/bot/dates?what=Dates&d=09/09/1998
-
-
-// const express = require('express')
-// const cors = require('cors')
-// const path = require('path')
-// const cookieParser = require('cookie-parser')
-// const Hebcal = require('hebcal')
-// const http = require('http')
-
-// const app = express()
-// app.use(express.json())
-
-// // Config the Express App
-// app.use(express.static('public'))
-// app.use(cookieParser())
-// app.use(express.json())
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
-//     const corsOptions = {
-//         origin: [
-//             'http://127.0.0.1:8080',
-//             'http://localhost:8080',
-//             'http://127.0.0.1:3000',
-//             'http://localhost:3000'
-//         ],
-//         credentials: true
-//     }
-//     app.use(cors(corsOptions))
-// }
-
-// // const botRoutes = require('./api/bot/bot.routes')
-// // app.use('/api/bot', botRoutes)
-
-// app.get('/bot/dates', async (req, res) => {
-//     try {
-//         const userInputDate = req.query.d
-//         const date = new Date(userInputDate)
-//         const hebDate = Hebcal.HDate(date)
-//         const torahPortion = hebDate.getSedra('he', true)
-
-//         const responseText = `התאריך המקביל: ${hebDate.toString('h')}  ${torahPortion}`
-//         const responseData = {
-//             "actions": [{ "type": "SendMessage", "text": responseText }]
-//         }
-//         res.json(responseData)
-//     } catch (error) {
-//         console.error(error)
-//         res.status(500).send('An error occurred while retrieving the Torah portion.')
-//     }
-// })
-
-// const port = 3030
-// app.listen(port, () =>
-//     console.log(`Server is ready at ${port}`)
-// )
