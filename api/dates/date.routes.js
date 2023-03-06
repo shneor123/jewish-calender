65ff24bd39d5a32751516091b93abad25d535183
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2023-03-06 10:02:14 +0200

    change bac k to all

--- api/dates/date.routes.js
+++ api/dates/date.routes.js
@@ -3,13 +3,13 @@ const { jewishService } = require('./date.controller')
 const router = express.Router()
 
 router.all('/', jewishService.getConvertDate)
-// router.all('/gHeDate', jewishService.getHeDateTodey)
+router.all('/gHeDate', jewishService.getHeDateTodey)
 router.all('/gDate', jewishService.getDateTodey)
 
 
-router.all('/date', jewishService.convertHebreow)
-router.all('/date/gre', jewishService.convertGregotrian)
-router.all('/zmanim/he', jewishService.heDateTodey)
-router.all('/zmanim', jewishService.gDateTodey)
+router.post('/date', jewishService.convertHebreow)
+router.post('/date/gre', jewishService.convertGregotrian)
+router.post('/zmanim/he', jewishService.heDateTodey)
+router.post('/zmanim', jewishService.gDateTodey)
 
 module.exports = router
